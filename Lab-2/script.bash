f7fc13492c108abbcaad8020ed505dd3622ab10f
Alexis416 <145903941+Alexis416@users.noreply.github.com>
2024-03-19 23:05:17 +0300

    Create script.bash

---
+++ Lab-2/script.bash
@@ -0,0 +1,22 @@
+#!bin/bash
+a=$1
+array_num=$(echo $a | tr "." "\n")
+perevod_num ()
+{
+	num=$1
+	a=
+	while [ $num -gt 0 ]
+	do
+		b=$(( $num % 2 ))
+		a="$b$a"
+		num=$(( $num / 2 ))
+	done
+	echo $a
+}
+str_num=
+for i in ${array_num[@]}
+do
+	bin_num=$(printf "%08d" $(perevod_num $i))
+	str_num="$str_num$bin_num."
+done
+echo "${str_num%.}"
